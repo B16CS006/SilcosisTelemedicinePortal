b27a9bb98dd87cecf296dee8dd2c8921de23e0ee
b16cs006 <meena.13@iitj.ac.in>
2019-11-22 14:46:18 +0530

    database is added

--- config/configuration.js
+++ config/configuration.js
@@ -1,9 +1,9 @@
 
 module.exports = {
     databaseConfig: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'Telemedicine'
+        host: 'us-cdbr-iron-east-05.cleardb.net',
+        user: 'b7be05e3d5eebc',
+        password: 'c7070ba3',
+        database: 'heroku_6ab45edb03359c4'
     }
 }

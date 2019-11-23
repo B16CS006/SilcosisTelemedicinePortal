0cebbd4da39b03247f6a491f1993b861c8f71414
b16cs006 <meena.13@iitj.ac.in>
2019-11-23 07:29:43 +0530

    changing config file but not working

--- config/configuration.js
+++ config/configuration.js
@@ -1,18 +1,18 @@
 
-module.exports = {
-    databaseConfig: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'Telemedicine'
-    }
-}
-
 // module.exports = {
 //     databaseConfig: {
-//         host: 'us-cdbr-iron-east-05.cleardb.net',
-//         user: 'b7be05e3d5eebc',
-//         password: 'c7070ba3',
-//         database: 'heroku_6ab45edb03359c4'
+//         host: 'localhost',
+//         user: 'root',
+//         password: '',
+//         database: 'Telemedicine'
 //     }
 // }
+
+module.exports = {
+    databaseConfig: {
+        host: 'us-cdbr-iron-east-05.cleardb.net',
+        user: 'b7be05e3d5eebc',
+        password: 'c7070ba3',
+        database: 'heroku_6ab45edb03359c4'
+    }
+}
--- database/connection.js
+++ database/connection.js
@@ -5,9 +5,9 @@ var execQuery = function(sql, data, callback){
     const connection = mysql.createConnection(databaseConfig)
     connection.connect()
     connection.query(sql, data, (err, result) => {
-        connection.end()
         callback(err, result);
     })
+    connection.end()
 }
 
 module.exports = execQuery
